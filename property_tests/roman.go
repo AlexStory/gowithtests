4c19fc6df7ac7d3f00e261395cc87c17436be1f8
Alex Story <astory11@yahoo.com>
2023-02-09 12:39:58 -0600

    finishd property based testing

---
+++ property_tests/roman.go
@@ -0,0 +1,48 @@
+package main
+
+import "strings"
+
+type RomanNumerals []RomanNumeral
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+func (r RomanNumerals) ValueOf(symbol string) int {
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return s.Value
+		}
+	}
+
+	return 0
+}
+
+var allRomanNumerals = RomanNumerals{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func ConvertToRoman(arabic int) string {
+	var result strings.Builder
+
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
+		}
+	}
+
+	return result.String()
+}
